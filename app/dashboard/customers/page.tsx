068242ce93aeeb83c01d9f10ce4b75c5d8140c48
Your Name <you@example.com>
2025-05-14 19:43:18 +0300

    new try

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,15 +1,25 @@
-
-import CustomersTable from "@/app/ui/customers/table";
-import { Metadata } from "next";
-export default function page() {
-    return (
-        <>
-        <p> Customer Page</p>
-        <CustomersTable customers={[]} />
-        </>
-    )
-};
+import { Suspense } from 'react';
+import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import CustomersTableSkeleton from '@/app/ui/skeletons';
+import Search from '@/app/ui/search';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Customers',
+    title:'Customers'
+}
+
+export default function Page() {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <Suspense fallback={<CustomersTableSkeleton />}>
+        <Search placeholder="Search customers..." />
+        {/* TODO: Replace the empty array with actual customers data */}
+        <CustomersTable customers={[]} />
+      </Suspense>
+    </div>
+  );
 }
